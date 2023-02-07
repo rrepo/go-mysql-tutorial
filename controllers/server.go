d84385ae12901b89f7d06355245ec5b0775ea035
repo <reporepo254@gmail.com>
2023-02-07 19:40:15 +0900

    test

--- controllers/server.go
+++ controllers/server.go
@@ -4,13 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
-func Server(){
+func Server() {
 	router := gin.Default()
 	router.GET("/albums", GetAlbums)
 	router.GET("/albums/:id", GetAlbumByID)
 	router.GET("/albums/delete/:id", DeleteAlbumByID)
+	router.POST("/albums", PostAlbums)
 	router.POST("/albums/update", UpdateAlbums)
-	
+
 	router.Run("localhost:8080")
 }
